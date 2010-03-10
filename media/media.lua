282f98a723cb5eed576b986efa0812f9f0353c6f
John <john.d.mann@gmail.com>
2010-03-10 01:13:04 -0600

    Remove unused fonts.

--- media/media.lua
+++ media/media.lua
@@ -5,7 +5,3 @@ ns.media = {
 	statusBar = [[Interface\Addons\recUI\media\texture\minimalist]],
 	font = [[Interface\Addons\recUI\media\font\rexlia free.ttf]]
 }
-local neuropol   = [[Interface\Addons\recUI\media\font\NEUROPOL.ttf]]
-local sonysketch = [[Interface\Addons\recUI\media\font\Sony_Sketch_EF.ttf]]
-local rexlia     = [[Interface\Addons\recUI\media\font\rexlia free.ttf]]
-local russell    = [[Interface\Addons\recUI\media\font\Russel Square LT.ttf]]
