15973afcdf51df3fa9a62b469d3b846936901b12
John <john.d.mann@gmail.com>
2010-03-09 22:56:39 -0600

    Initial Import

---
+++ media/media.lua
@@ -0,0 +1,4 @@
+local neuropol   = [[Interface\Addons\recUI\media\font\NEUROPOL.ttf]]
+local sonysketch = [[Interface\Addons\recUI\media\font\Sony_Sketch_EF.ttf]]
+local rexlia     = [[Interface\Addons\recUI\media\font\rexlia free.ttf]]
+local russell    = [[Interface\Addons\recUI\media\font\Russel Square LT.ttf]]
