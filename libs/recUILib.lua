4dc4c032d583b86bd9a247cb5f2bc6d7b033b024
John <john.d.mann@gmail.com>
2010-03-11 02:41:03 -0600

    Submerge!  Added playerClass.

--- libs/recUILib.lua
+++ libs/recUILib.lua
@@ -1,18 +1,23 @@
-local _, ns = ...
+local _, recUI = ...
 
-ns.NullFunction = function() end
+recUI.lib = {
 
-ns.Kill = function(object)
-	local object_reference = object
-	if type(object) == "string" then
-		object_reference = _G[object]
-	else
-		object_reference = object
+	NullFunction = function() end,
+
+	Kill = function(object)
+		local object_reference = object
+		if type(object) == "string" then
+			object_reference = _G[object]
+		else
+			object_reference = object
+		end
+		if not object_reference then return end
+		if type(object_reference) == "frame" then
+			object_reference:UnregisterAllEvents()
+		end
+		object_reference.Show = ns.NullFunction
+		object_reference:Hide()
 	end
-	if not object_reference then return end
-	if type(object_reference) == "frame" then
-		object_reference:UnregisterAllEvents()
-	end
-	object_reference.Show = ns.NullFunction
-	object_reference:Hide()
-end
+}
+
+recUI.lib.playerClass = select(2, UnitClass("player")
