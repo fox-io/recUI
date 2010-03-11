17693931dfa6d14da9833b568ae44b3513bb3e5a
John <john.d.mann@gmail.com>
2010-03-11 16:48:14 -0600

    Use recUI.lib.events

--- media/media.lua
+++ media/media.lua
@@ -85,68 +85,59 @@ NAMEPLATE_FONT     = recUI.media.font
 DAMAGE_TEXT_FONT   = recUI.media.font
 STANDARD_TEXT_FONT = recUI.media.font
 
-recUI.font_events = CreateFrame("Frame")
-recUI.font_events:RegisterEvent("ADDON_LOADED")
+recUI.lib.registerEvent("ADDON_LOADED", "recUIMediaLoadFonts", function(self, event, addon)
+	if IsAddOnLoaded("Blizzard_CombatLog") or addon == "Blizzard_CombatLog" then
+		UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT = 9
+		CHAT_FONT_HEIGHTS = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
 
-recUI.font_events:SetScript("OnEvent", function(self, event, addon)
-	
-	--if IsAddOnLoaded("Blizzard_CombatLog") or addon == "Blizzard_CombatLog" then
-	
-	UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT = 9
-	CHAT_FONT_HEIGHTS = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
+		-- Base fonts
+		SetFont(AchievementFont_Small,              recUI.media.font, 9)
+		SetFont(GameFontNormalHuge,					recUI.media.font, 20, "OUTLINE") -- Used for RaidWarningFrame
+		SetFont(GameTooltipHeader,                  recUI.media.font, 10, "OUTLINE")
+		SetFont(InvoiceFont_Med,                    recUI.media.font, 9, nil, 0.15, 0.09, 0.04)
+		SetFont(InvoiceFont_Small,                  recUI.media.font, 8,  nil, 0.15, 0.09, 0.04)
+		SetFont(MailFont_Large,                     recUI.media.font, 9, nil, 0.15, 0.09, 0.04, 0.54, 0.4, 0.1, 1, -1)
+		SetFont(NumberFont_OutlineThick_Mono_Small, recUI.media.font, 11, "THICKOUTLINE")
+		SetFont(NumberFont_Outline_Huge,            recUI.media.font, 13, "OUTLINE", 28)
+		SetFont(NumberFont_Outline_Large,           recUI.media.font, 13, "OUTLINE")
+		SetFont(NumberFont_Outline_Med,             recUI.media.font, 13, "OUTLINE")
+		SetFont(NumberFont_Shadow_Med,              recUI.media.font, 12)
+		SetFont(NumberFont_Shadow_Small,            recUI.media.font, 10)
+		SetFont(QuestFont_Large,                    recUI.media.font, 13)
+		SetFont(QuestFont_Shadow_Huge,              recUI.media.font, 13, nil, nil, nil, nil, 0.54, 0.4, 0.1)
+		SetFont(ReputationDetailFont,               recUI.media.font, 10, nil, nil, nil, nil, 0, 0, 0, 1, -1)
+		SetFont(SpellFont_Small,                    recUI.media.font, 9)
+		SetFont(SystemFont_InverseShadow_Small,     recUI.media.font, 9)
+		SetFont(SystemFont_Large,                   recUI.media.font, 13)
+		SetFont(SystemFont_Med1,                    recUI.media.font, 11)
+		SetFont(SystemFont_Med2,                    recUI.media.font, 12, nil, 0.15, 0.09, 0.04)
+		SetFont(SystemFont_Med3,                    recUI.media.font, 13)
+		SetFont(SystemFont_OutlineThick_Huge2,      recUI.media.font, 13, "OUTLINE")
+		SetFont(SystemFont_OutlineThick_Huge4,      recUI.media.font, 13, "OUTLINE")
+		SetFont(SystemFont_OutlineThick_WTF,        recUI.media.font, 13, "OUTLINE", nil, nil, nil, 0, 0, 0, 1, -1)
+		SetFont(SystemFont_Outline_Small,           recUI.media.font, 11, "OUTLINE")
+		SetFont(SystemFont_Shadow_Huge1,            recUI.media.font, 13)
+		SetFont(SystemFont_Shadow_Huge3,            recUI.media.font, 13)
+		SetFont(SystemFont_Shadow_Large,            recUI.media.font, 13)
+		SetFont(SystemFont_Shadow_Med1,             recUI.media.font, 11)
+		SetFont(SystemFont_Shadow_Med3,             recUI.media.font, 13)
+		SetFont(SystemFont_Shadow_Outline_Huge2,    recUI.media.font, 13, "OUTLINE")
+		SetFont(SystemFont_Shadow_Small,            recUI.media.font, 9)
+		SetFont(SystemFont_Small,                   recUI.media.font, 8)
+		SetFont(SystemFont_Tiny,                    recUI.media.font, 7)
+		SetFont(Tooltip_Med,                        recUI.media.font, 11)
+		SetFont(Tooltip_Small,                      recUI.media.font, 10)
 
-	-- Base fonts
-	SetFont(AchievementFont_Small,              recUI.media.font, 9)
-	SetFont(GameFontNormalHuge,					recUI.media.font, 20, "OUTLINE") -- Used for RaidWarningFrame
-	SetFont(GameTooltipHeader,                  recUI.media.font, 10, "OUTLINE")
-	SetFont(InvoiceFont_Med,                    recUI.media.font, 9, nil, 0.15, 0.09, 0.04)
-	SetFont(InvoiceFont_Small,                  recUI.media.font, 8,  nil, 0.15, 0.09, 0.04)
-	SetFont(MailFont_Large,                     recUI.media.font, 9, nil, 0.15, 0.09, 0.04, 0.54, 0.4, 0.1, 1, -1)
-	SetFont(NumberFont_OutlineThick_Mono_Small, recUI.media.font, 11, "THICKOUTLINE")
-	SetFont(NumberFont_Outline_Huge,            recUI.media.font, 13, "OUTLINE", 28)
-	SetFont(NumberFont_Outline_Large,           recUI.media.font, 13, "OUTLINE")
-	SetFont(NumberFont_Outline_Med,             recUI.media.font, 13, "OUTLINE")
-	SetFont(NumberFont_Shadow_Med,              recUI.media.font, 12)
-	SetFont(NumberFont_Shadow_Small,            recUI.media.font, 10)
-	SetFont(QuestFont_Large,                    recUI.media.font, 13)
-	SetFont(QuestFont_Shadow_Huge,              recUI.media.font, 13, nil, nil, nil, nil, 0.54, 0.4, 0.1)
-	SetFont(ReputationDetailFont,               recUI.media.font, 10, nil, nil, nil, nil, 0, 0, 0, 1, -1)
-	SetFont(SpellFont_Small,                    recUI.media.font, 9)
-	SetFont(SystemFont_InverseShadow_Small,     recUI.media.font, 9)
-	SetFont(SystemFont_Large,                   recUI.media.font, 13)
-	SetFont(SystemFont_Med1,                    recUI.media.font, 11)
-	SetFont(SystemFont_Med2,                    recUI.media.font, 12, nil, 0.15, 0.09, 0.04)
-	SetFont(SystemFont_Med3,                    recUI.media.font, 13)
-	SetFont(SystemFont_OutlineThick_Huge2,      recUI.media.font, 13, "OUTLINE")
-	SetFont(SystemFont_OutlineThick_Huge4,      recUI.media.font, 13, "OUTLINE")
-	SetFont(SystemFont_OutlineThick_WTF,        recUI.media.font, 13, "OUTLINE", nil, nil, nil, 0, 0, 0, 1, -1)
-	SetFont(SystemFont_Outline_Small,           recUI.media.font, 11, "OUTLINE")
-	SetFont(SystemFont_Shadow_Huge1,            recUI.media.font, 13)
-	SetFont(SystemFont_Shadow_Huge3,            recUI.media.font, 13)
-	SetFont(SystemFont_Shadow_Large,            recUI.media.font, 13)
-	SetFont(SystemFont_Shadow_Med1,             recUI.media.font, 11)
-	SetFont(SystemFont_Shadow_Med3,             recUI.media.font, 13)
-	SetFont(SystemFont_Shadow_Outline_Huge2,    recUI.media.font, 13, "OUTLINE")
-	SetFont(SystemFont_Shadow_Small,            recUI.media.font, 9)
-	SetFont(SystemFont_Small,                   recUI.media.font, 8)
-	SetFont(SystemFont_Tiny,                    recUI.media.font, 7)
-	SetFont(Tooltip_Med,                        recUI.media.font, 11)
-	SetFont(Tooltip_Small,                      recUI.media.font, 10)
-
-	-- Derived fonts
-	SetFont(BossEmoteNormalHuge,                recUI.media.font, 13, "OUTLINE")
-	SetFont(CombatTextFont,                     recUI.media.font, 13)
-	SetFont(ErrorFont,                          recUI.media.font, 13, "OUTLINE")
-	SetFont(QuestFontNormalSmall,               recUI.media.font, 11, nil, nil, nil, nil, 0.54, 0.4, 0.1)
-	SetFont(WorldMapTextFont,                   recUI.media.font, 13, "THICKOUTLINE",  38, nil, nil, 0, 0, 0, 1, -1)
-	
-	hooksecurefunc("PlayerTitleFrame_UpdateTitles", FixTitleFont)
-	FixTitleFont()
-	
-	SetFont = nil
-	self:SetScript("OnEvent", nil)
-	self:UnregisterAllEvents()
-	self = nil
-	
-	--end
-end)--]]
+		-- Derived fonts
+		SetFont(BossEmoteNormalHuge,                recUI.media.font, 13, "OUTLINE")
+		SetFont(CombatTextFont,                     recUI.media.font, 13)
+		SetFont(ErrorFont,                          recUI.media.font, 13, "OUTLINE")
+		SetFont(QuestFontNormalSmall,               recUI.media.font, 11, nil, nil, nil, nil, 0.54, 0.4, 0.1)
+		SetFont(WorldMapTextFont,                   recUI.media.font, 13, "THICKOUTLINE",  38, nil, nil, 0, 0, 0, 1, -1)
+		
+		hooksecurefunc("PlayerTitleFrame_UpdateTitles", FixTitleFont)
+		FixTitleFont()
+		
+		recUI.lib.unregisterEvent("recUIMediaLoadFonts")
+	end
+end)
