724515ef061726c3b4fd0f7572d81d5e43bca63d
John <john.d.mann@gmail.com>
2010-03-11 17:27:02 -0600

    Use recUILib for onload event.

--- modules/chat.lua
+++ modules/chat.lua
@@ -122,279 +122,277 @@ local _, playerClass = UnitClass("player")
 local c = RAID_CLASS_COLORS[playerClass]
 
 local previous_zone
-local function OnEvent(self, event, ...)
-	if ... == "Blizzard_CombatLog" then
-		f:UnregisterEvent("ADDON_LOADED")
+recUI.lib.registerEvent("VARIABLES_LOADED", "recUIModuleChatOnLoad", function(self, event, addon, ...)
+	-- Fake a PEW to this handler to prevent an error on game clients which have a fresh WTF folder.
+	-- This forces an init on the checkboxes in the chatframe config pages.  For some reason, blocking
+	-- the default chat tabs prevents this from being called correctly.
+	ChatConfigFrame_OnEvent(nil, "PLAYER_ENTERING_WORLD", ...)
+	
+	local cf1 = _G["ChatFrame1"]
+	local cfmb = _G["ChatFrameMenuButton"]
+	
+	-- Rename Dungeon Guide to 'P' just like normal party people. Has lighter color, tho.
+	CHAT_PARTY_GUIDE_GET = "|Hchannel:party|hP|h %s:\32";
+
+	-- Remove chat frame mouseover highlight. Thanks Caellian
+	DEFAULT_CHATFRAME_ALPHA = 0
+
+	for i = 1, 7 do
+		local cf = _G[format("ChatFrame%s", i)]
 		
-		-- Fake a PEW to this handler to prevent an error on game clients which have a fresh WTF folder.
-		-- This forces an init on the checkboxes in the chatframe config pages.  For some reason, blocking
-		-- the default chat tabs prevents this from being called correctly.
-		ChatConfigFrame_OnEvent(nil, "PLAYER_ENTERING_WORLD", ...)
+		cf:SetParent(TestTextureFrame)
 		
-		local cf1 = _G["ChatFrame1"]
-		local cfmb = _G["ChatFrameMenuButton"]
+		cf:SetFont(recUI.media.font, 9, nil)
 		
-		-- Rename Dungeon Guide to 'P' just like normal party people. Has lighter color, tho.
-		CHAT_PARTY_GUIDE_GET = "|Hchannel:party|hP|h %s:\32";
+		-- Disable/Hide chat tabs
+		local cft = _G[format("ChatFrame%sTab", i)]
+		local cftf = _G[format("ChatFrame%sTabFlash", i)]
+		cf:SetScript("OnEnter", nil)
+		cft:EnableMouse(false)
+		cft:SetScript("OnEnter", nil)
+		cft:SetScript("OnLeave", nil)
+		cft:GetHighlightTexture():SetTexture(nil)
+		cft.SetAlpha = function() end
+		cftf:SetScript("OnShow", nil)
+		cftf:SetScript("OnHide", nil)
+		cftf:GetRegions():SetTexture(nil)
+		recUI.lib.Kill(_G[format("ChatFrame%sTabLeft", i)])
+		recUI.lib.Kill(_G[format("ChatFrame%sTabMiddle", i)])
+		recUI.lib.Kill(_G[format("ChatFrame%sTabRight", i)])
+		recUI.lib.Kill(_G[format("ChatFrame%sTabText", i)])
+		recUI.lib.Kill(_G["ChatFrame"..i.."TabDockRegionHighlight"])
 
-		-- Remove chat frame mouseover highlight. Thanks Caellian
-		DEFAULT_CHATFRAME_ALPHA = 0
-
-		for i = 1, 7 do
-			local cf = _G[format("ChatFrame%s", i)]
-			
-			cf:SetParent(TestTextureFrame)
-			
-			cf:SetFont(recUI.media.font, 9, nil)
-			
-			-- Disable/Hide chat tabs
-			local cft = _G[format("ChatFrame%sTab", i)]
-			local cftf = _G[format("ChatFrame%sTabFlash", i)]
-			cf:SetScript("OnEnter", nil)
-			cft:EnableMouse(false)
-			cft:SetScript("OnEnter", nil)
-			cft:SetScript("OnLeave", nil)
-			cft:GetHighlightTexture():SetTexture(nil)
-			cft.SetAlpha = function() end
-			cftf:SetScript("OnShow", nil)
-			cftf:SetScript("OnHide", nil)
-			cftf:GetRegions():SetTexture(nil)
-			recUI.lib.Kill(_G[format("ChatFrame%sTabLeft", i)])
-			recUI.lib.Kill(_G[format("ChatFrame%sTabMiddle", i)])
-			recUI.lib.Kill(_G[format("ChatFrame%sTabRight", i)])
-			recUI.lib.Kill(_G[format("ChatFrame%sTabText", i)])
-			recUI.lib.Kill(_G["ChatFrame"..i.."TabDockRegionHighlight"])
-
-			-- Enable Mousewheel scrolling
-			cf:SetScript("OnMouseWheel", function(self, v)
-				if v > 0 then
-					if IsShiftKeyDown() then
-						self:ScrollToTop()
-					else
-						self:ScrollUp()
-					end
-				elseif v < 0 then
-					if IsShiftKeyDown() then
-						self:ScrollToBottom()
-					else
-						self:ScrollDown()
-					end
-				end
-			end)
-			cf:EnableMouseWheel(true)
-
-			-- Disable side scroll buttons
-			recUI.lib.Kill(_G[format("ChatFrame%sUpButton", i)])
-			recUI.lib.Kill(_G[format("ChatFrame%sDownButton", i)])
-			recUI.lib.Kill(_G[format("ChatFrame%sBottomButton", i)])
-
-			-- Disable fading
-			cf:SetFading(nil)
-
-			-- Larger scrollback buffer (x10)
-			cf:SetMaxLines(2500)
-			
-			remove_chat(i)
-			enable_chat_types(i)
-
-			cf:Hide()
-		end
-		
-		join_zone_channels()
-
-		-- Move and dim language/emote button
-		cfmb:SetAlpha(0.125)
-		cfmb:ClearAllPoints()
-		cfmb:SetPoint("TOPRIGHT", RBoxPanel, "TOPRIGHT", 0, 0)
-
-		-- Custom chat tabs
-		cftbb:SetWidth(180); cftbb:SetHeight(20)
-		cftbb:SetPoint("BOTTOMLEFT", cf1, "TOPLEFT", 0, 0)
-		cftbb:EnableMouse(true)
-		cftbb:SetAlpha(1)
-		cftbb.id = 1
-		
-		local function MakeButton(id, txt, tip)
-			local btn = CreateFrame("Button", format("RecChatButton%s", id), cftbb)
-			btn.id = id
-			btn:SetWidth(30); btn:SetHeight(20)
-			--btn:SetScript("OnEnter", function(...) RecChatButtonBar:SetAlpha(1) end)
-			--btn:SetScript("OnLeave", function(...) RecChatButtonBar:SetAlpha(0) end)
-			btn:RegisterForClicks("LeftButtonDown", "RightButtonDown")
-			btn:SetScript("OnClick", function(self, button, ...)
-				if button == "RightButton" then
-					if self.id == RecChatButtonBar.id then
-						ShowUIPanel(ChatConfigFrame)
-					end
+		-- Enable Mousewheel scrolling
+		cf:SetScript("OnMouseWheel", function(self, v)
+			if v > 0 then
+				if IsShiftKeyDown() then
+					self:ScrollToTop()
 				else
-					ShowChatFrame(self)
+					self:ScrollUp()
 				end
-			end)
-			if tip then
-				btn:SetScript("OnEnter", function(self)
-					GameTooltip:SetOwner(self, "ANCHOR_NONE")
-					GameTooltip:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT")
-					GameTooltip:AddLine(tip)
-					GameTooltip:Show()
-				end)
-			end
-			btn:SetScript("OnLeave", function()
-				GameTooltip:Hide()
-			end)
-			btn.t = btn:CreateFontString(nil, "OVERLAY")
-			btn.t:SetFont(recUI.media.font, 9, nil)
-			btn.t:SetPoint("CENTER")
-			btn.t:SetTextColor(1, 1, 1)
-			btn.t:SetText(txt)
-			
-			return btn
-		end
-		
-		local cft1 = MakeButton(1, "G", "General")
-		local cft2 = MakeButton(2, "C", "Combat")
-		local cft3 = MakeButton(3, "I", "Info")
-		
-		cft3:SetPoint("BOTTOMRIGHT", RBoxPanel, "TOPRIGHT", 0, 0)
-		cft2:SetPoint("RIGHT", cft3, "LEFT")
-		cft1:SetPoint("RIGHT", cft2, "LEFT")
-
-		-- Override old tab bar functions so that we can use our custom buttons to open chat options
-		FCF_GetCurrentChatFrameID = GetChatFrameID
-		FCF_GetCurrentChatFrame = GetCurrentChatFrame
-
-		-- Edit box positioning
-		local cfeb = _G["ChatFrameEditBox"]
-		cfeb:SetFrameStrata("TOOLTIP")
-		cfeb:ClearAllPoints()
-		cfeb:SetPoint("BOTTOMLEFT", RBoxPanel, "TOPLEFT", 0, 0)
-		cfeb:SetPoint("BOTTOMRIGHT", cft1, "BOTTOMLEFT", 0, 0)
-
-		-- Edit box theme
-		local l, m, r = select(6, cfeb:GetRegions())
-		l:Hide()
-		m:Hide()
-		r:Hide()
-		cfeb:SetFont(recUI.media.font, 9, nil)
-		cfeb:SetHeight(20)
-		cfeb:HookScript("OnShow", function(self)
-			color_border(EditBoxPanel)
-		end)
-		cfeb:HookScript("OnHide", function(self)
-			EditBoxPanel:SetBackdropBorderColor(0, 0, 0)
-			-- Change the color of the selected chat button to match sticky chat type
-			for i=1,3 do
-				if _G[format("RecChatButton%s", i)].id == RecChatButtonBar.id then
-					color_border(_G[format("ChatTab%sPanel", i)])
+			elseif v < 0 then
+				if IsShiftKeyDown() then
+					self:ScrollToBottom()
+				else
+					self:ScrollDown()
 				end
 			end
 		end)
+		cf:EnableMouseWheel(true)
 
-		-- Allow arrow key editing
-		cfeb:SetAltArrowKeyMode(0)
+		-- Disable side scroll buttons
+		recUI.lib.Kill(_G[format("ChatFrame%sUpButton", i)])
+		recUI.lib.Kill(_G[format("ChatFrame%sDownButton", i)])
+		recUI.lib.Kill(_G[format("ChatFrame%sBottomButton", i)])
 
-		-- Enable edit box history
-		local history = {}
-		local function AddHistory(obj, line)
-			table.insert(history, line)
-			for i=1, #history - obj:GetHistoryLines() do
-				table.remove(history, 1)
-			end
-		end
-		cfeb:HookScript("OnEnter", AddHistory)
+		-- Disable fading
+		cf:SetFading(nil)
 
-		-- Chat timestamps
-		--local PostTimestamp = cf1.AddMessage
-		--local function AddTimestamp(frame, text, ...)
-			--if text then text = format("[%s] %s", date("%H:%M"), text) end
-			--return PostTimestamp(frame, text, ...)
-		--end
-		--cf1.AddMessage = AddTimestamp
+		-- Larger scrollback buffer (x10)
+		cf:SetMaxLines(2500)
 		
-		-- Shorten channel names/messages
-		CHAT_PARTY_GET               = "|Hchannel:Party|hP|h %s:\32"
-		CHAT_MONSTER_PARTY_GET       = "|Hchannel:Party|hP|h %s: "
-		CHAT_PARTY_LEADER_GET        = "|Hchannel:Party|hP|h %s: "
-		CHAT_CHANNEL_LEAVE_GET       = "%s left."
-		CHAT_MONSTER_SAY_GET         = "%s: "
-		CHAT_CHANNEL_JOIN_GET        = "%s joined."
-		CHAT_BATTLEGROUND_LEADER_GET = "|Hchannel:Battleground|hBL|h %s: "
-		CHAT_RAID_GET                = "|Hchannel:raid|hR|h %s: "
-		CHAT_YELL_GET                = "%s: "
-		CHAT_BATTLEGROUND_GET        = "|Hchannel:Battleground|hBG|h %s: "
-		CHAT_GUILD_GET               = "|Hchannel:Guild|hG|h %s: "
-		CHAT_SAY_GET                 = "%s: "
-		CHAT_RAID_WARNING_GET        = "RW %s: "
-		CHAT_RAID_LEADER_GET         = "|Hchannel:raid|hRL|h %s: "
-		CHAT_OFFICER_GET             = "|Hchannel:o|hO|h %s: "
-		CHAT_MONSTER_YELL_GET        = "%s: "
-		CHAT_MONSTER_WHISPER_GET     = "From %s: "
-		CHAT_CHANNEL_LIST_GET        = "|Hchannel:%d|h%s|h:\32"
-		CHAT_YOU_CHANGED_NOTICE      = ""--"Changed: |Hchannel:%d|h%s|h"
-		CHAT_YOU_JOINED_NOTICE       = ""--"Joined: |Hchannel:%d|h%s|h"
-		CHAT_YOU_LEFT_NOTICE         = ""--"Left: |Hchannel:%d|h%s|h"
-		CHAT_SUSPENDED_NOTICE        = ""--"Left: |Hchannel:%d|h%s|h "
-		ERR_FRIEND_OFFLINE_S         = "%s logged out."
-		ERR_FRIEND_ONLINE_SS         = "|Hplayer:%s|h%s|h logged in."
-		ERR_FRIEND_REMOVED_S         = "%s is no longer your friend."
-		ERR_FRIEND_ADDED_S           = "%s is now your friend."
-		
-		-- Shorten edit box targets
-		CHAT_BATTLEGROUND_SEND = "BG:\32"
-		CHAT_CHANNEL_SEND      = "%d. %s:\32"
-		CHAT_GUILD_SEND        =  "G:\32"
-		CHAT_OFFICER_SEND      =  "O:\32"
-		CHAT_PARTY_SEND        =  "P:\32"
-		CHAT_RAID_SEND         =  "R:\32"
-		CHAT_RAID_WARNING_SEND = "RW:\32"
-		CHAT_SAY_SEND          =  "S:\32"
-		CHAT_WHISPER_SEND      = "%s:\32"
-		CHAT_YELL_SEND         =  "Y:\32"
+		remove_chat(i)
+		enable_chat_types(i)
 
-		-- Move chat into proper position
-		for cfnum = 1, 2 do
-			if cfnum == 2 then cfnum = 3 end
-			local cf = _G[format("ChatFrame%d", cfnum)]
-			cf:ClearAllPoints()
-			cf:SetPoint("BOTTOMLEFT", RBoxPanel, "BOTTOMLEFT", 5, 7)
-			cf:SetPoint("TOPRIGHT", RBoxPanel, "TOPRIGHT", -5, -7)
-			cf.SetPoint = function() end
-			cf:Hide()
-		end
-
-		for i = 1, 7 do
-			-- Make chat frames transparent
-			_G[format("ChatFrame%sBackground", i)]:SetVertexColor(0, 0, 0, 0)
-			_G[format("ChatFrame%sBackground", i)].SetVertexColor = function() end
-		end
-		
-		-- Enable class coloring
-		ToggleChatColorNamesByClassGroup(true, "SAY")
-		ToggleChatColorNamesByClassGroup(true, "EMOTE")
-		ToggleChatColorNamesByClassGroup(true, "YELL")
-		ToggleChatColorNamesByClassGroup(true, "GUILD")
-		ToggleChatColorNamesByClassGroup(true, "GUILD_OFFICER")
-		ToggleChatColorNamesByClassGroup(true, "GUILD_ACHIEVEMENT")
-		ToggleChatColorNamesByClassGroup(true, "WHISPER")
-		ToggleChatColorNamesByClassGroup(true, "PARTY")
-		ToggleChatColorNamesByClassGroup(true, "PARTY_LEADER")
-		ToggleChatColorNamesByClassGroup(true, "RAID")
-		ToggleChatColorNamesByClassGroup(true, "RAID_LEADER")
-		ToggleChatColorNamesByClassGroup(true, "RAID_WARNING")
-		ToggleChatColorNamesByClassGroup(true, "BATTLEGROUND")
-		ToggleChatColorNamesByClassGroup(true, "BATTLEGROUND_LEADER")
-		ToggleChatColorNamesByClassGroup(true, "ACHIEVEMENT")
-		
-		-- Start with chat frame 1 shown.
-		ShowChatFrame(cft1)
-
-		-- Prevent Blizzard from changing to chat tab 1 (on instance enter, flight path end etc).
-		cf1:HookScript("OnShow", function()
-			if RecChatButtonBar.id ~= 1 then
-				ShowChatFrame(_G[format("RecChatButton%d", RecChatButtonBar.id)])
-			end
-		end)
+		cf:Hide()
 	end
-end
+	
+	join_zone_channels()
+
+	-- Move and dim language/emote button
+	cfmb:SetAlpha(0.125)
+	cfmb:ClearAllPoints()
+	cfmb:SetPoint("TOPRIGHT", RBoxPanel, "TOPRIGHT", 0, 0)
+
+	-- Custom chat tabs
+	cftbb:SetWidth(180); cftbb:SetHeight(20)
+	cftbb:SetPoint("BOTTOMLEFT", cf1, "TOPLEFT", 0, 0)
+	cftbb:EnableMouse(true)
+	cftbb:SetAlpha(1)
+	cftbb.id = 1
+	
+	local function MakeButton(id, txt, tip)
+		local btn = CreateFrame("Button", format("RecChatButton%s", id), cftbb)
+		btn.id = id
+		btn:SetWidth(30); btn:SetHeight(20)
+		--btn:SetScript("OnEnter", function(...) RecChatButtonBar:SetAlpha(1) end)
+		--btn:SetScript("OnLeave", function(...) RecChatButtonBar:SetAlpha(0) end)
+		btn:RegisterForClicks("LeftButtonDown", "RightButtonDown")
+		btn:SetScript("OnClick", function(self, button, ...)
+			if button == "RightButton" then
+				if self.id == RecChatButtonBar.id then
+					ShowUIPanel(ChatConfigFrame)
+				end
+			else
+				ShowChatFrame(self)
+			end
+		end)
+		if tip then
+			btn:SetScript("OnEnter", function(self)
+				GameTooltip:SetOwner(self, "ANCHOR_NONE")
+				GameTooltip:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT")
+				GameTooltip:AddLine(tip)
+				GameTooltip:Show()
+			end)
+		end
+		btn:SetScript("OnLeave", function()
+			GameTooltip:Hide()
+		end)
+		btn.t = btn:CreateFontString(nil, "OVERLAY")
+		btn.t:SetFont(recUI.media.font, 9, nil)
+		btn.t:SetPoint("CENTER")
+		btn.t:SetTextColor(1, 1, 1)
+		btn.t:SetText(txt)
+		
+		return btn
+	end
+	
+	local cft1 = MakeButton(1, "G", "General")
+	local cft2 = MakeButton(2, "C", "Combat")
+	local cft3 = MakeButton(3, "I", "Info")
+	
+	cft3:SetPoint("BOTTOMRIGHT", RBoxPanel, "TOPRIGHT", 0, 0)
+	cft2:SetPoint("RIGHT", cft3, "LEFT")
+	cft1:SetPoint("RIGHT", cft2, "LEFT")
+
+	-- Override old tab bar functions so that we can use our custom buttons to open chat options
+	FCF_GetCurrentChatFrameID = GetChatFrameID
+	FCF_GetCurrentChatFrame = GetCurrentChatFrame
+
+	-- Edit box positioning
+	local cfeb = _G["ChatFrameEditBox"]
+	cfeb:SetFrameStrata("TOOLTIP")
+	cfeb:ClearAllPoints()
+	cfeb:SetPoint("BOTTOMLEFT", RBoxPanel, "TOPLEFT", 0, 0)
+	cfeb:SetPoint("BOTTOMRIGHT", cft1, "BOTTOMLEFT", 0, 0)
+
+	-- Edit box theme
+	local l, m, r = select(6, cfeb:GetRegions())
+	l:Hide()
+	m:Hide()
+	r:Hide()
+	cfeb:SetFont(recUI.media.font, 9, nil)
+	cfeb:SetHeight(20)
+	cfeb:HookScript("OnShow", function(self)
+		color_border(EditBoxPanel)
+	end)
+	cfeb:HookScript("OnHide", function(self)
+		EditBoxPanel:SetBackdropBorderColor(0, 0, 0)
+		-- Change the color of the selected chat button to match sticky chat type
+		for i=1,3 do
+			if _G[format("RecChatButton%s", i)].id == RecChatButtonBar.id then
+				color_border(_G[format("ChatTab%sPanel", i)])
+			end
+		end
+	end)
+
+	-- Allow arrow key editing
+	cfeb:SetAltArrowKeyMode(0)
+
+	-- Enable edit box history
+	local history = {}
+	local function AddHistory(obj, line)
+		table.insert(history, line)
+		for i=1, #history - obj:GetHistoryLines() do
+			table.remove(history, 1)
+		end
+	end
+	cfeb:HookScript("OnEnter", AddHistory)
+
+	-- Chat timestamps
+	--local PostTimestamp = cf1.AddMessage
+	--local function AddTimestamp(frame, text, ...)
+		--if text then text = format("[%s] %s", date("%H:%M"), text) end
+		--return PostTimestamp(frame, text, ...)
+	--end
+	--cf1.AddMessage = AddTimestamp
+	
+	-- Shorten channel names/messages
+	CHAT_PARTY_GET               = "|Hchannel:Party|hP|h %s:\32"
+	CHAT_MONSTER_PARTY_GET       = "|Hchannel:Party|hP|h %s: "
+	CHAT_PARTY_LEADER_GET        = "|Hchannel:Party|hP|h %s: "
+	CHAT_CHANNEL_LEAVE_GET       = "%s left."
+	CHAT_MONSTER_SAY_GET         = "%s: "
+	CHAT_CHANNEL_JOIN_GET        = "%s joined."
+	CHAT_BATTLEGROUND_LEADER_GET = "|Hchannel:Battleground|hBL|h %s: "
+	CHAT_RAID_GET                = "|Hchannel:raid|hR|h %s: "
+	CHAT_YELL_GET                = "%s: "
+	CHAT_BATTLEGROUND_GET        = "|Hchannel:Battleground|hBG|h %s: "
+	CHAT_GUILD_GET               = "|Hchannel:Guild|hG|h %s: "
+	CHAT_SAY_GET                 = "%s: "
+	CHAT_RAID_WARNING_GET        = "RW %s: "
+	CHAT_RAID_LEADER_GET         = "|Hchannel:raid|hRL|h %s: "
+	CHAT_OFFICER_GET             = "|Hchannel:o|hO|h %s: "
+	CHAT_MONSTER_YELL_GET        = "%s: "
+	CHAT_MONSTER_WHISPER_GET     = "From %s: "
+	CHAT_CHANNEL_LIST_GET        = "|Hchannel:%d|h%s|h:\32"
+	CHAT_YOU_CHANGED_NOTICE      = ""--"Changed: |Hchannel:%d|h%s|h"
+	CHAT_YOU_JOINED_NOTICE       = ""--"Joined: |Hchannel:%d|h%s|h"
+	CHAT_YOU_LEFT_NOTICE         = ""--"Left: |Hchannel:%d|h%s|h"
+	CHAT_SUSPENDED_NOTICE        = ""--"Left: |Hchannel:%d|h%s|h "
+	ERR_FRIEND_OFFLINE_S         = "%s logged out."
+	ERR_FRIEND_ONLINE_SS         = "|Hplayer:%s|h%s|h logged in."
+	ERR_FRIEND_REMOVED_S         = "%s is no longer your friend."
+	ERR_FRIEND_ADDED_S           = "%s is now your friend."
+	
+	-- Shorten edit box targets
+	CHAT_BATTLEGROUND_SEND = "BG:\32"
+	CHAT_CHANNEL_SEND      = "%d. %s:\32"
+	CHAT_GUILD_SEND        =  "G:\32"
+	CHAT_OFFICER_SEND      =  "O:\32"
+	CHAT_PARTY_SEND        =  "P:\32"
+	CHAT_RAID_SEND         =  "R:\32"
+	CHAT_RAID_WARNING_SEND = "RW:\32"
+	CHAT_SAY_SEND          =  "S:\32"
+	CHAT_WHISPER_SEND      = "%s:\32"
+	CHAT_YELL_SEND         =  "Y:\32"
+
+	-- Move chat into proper position
+	for cfnum = 1, 2 do
+		if cfnum == 2 then cfnum = 3 end
+		local cf = _G[format("ChatFrame%d", cfnum)]
+		cf:ClearAllPoints()
+		cf:SetPoint("BOTTOMLEFT", RBoxPanel, "BOTTOMLEFT", 5, 7)
+		cf:SetPoint("TOPRIGHT", RBoxPanel, "TOPRIGHT", -5, -7)
+		cf.SetPoint = function() end
+		cf:Hide()
+	end
+
+	for i = 1, 7 do
+		-- Make chat frames transparent
+		_G[format("ChatFrame%sBackground", i)]:SetVertexColor(0, 0, 0, 0)
+		_G[format("ChatFrame%sBackground", i)].SetVertexColor = function() end
+	end
+	
+	-- Enable class coloring
+	ToggleChatColorNamesByClassGroup(true, "SAY")
+	ToggleChatColorNamesByClassGroup(true, "EMOTE")
+	ToggleChatColorNamesByClassGroup(true, "YELL")
+	ToggleChatColorNamesByClassGroup(true, "GUILD")
+	ToggleChatColorNamesByClassGroup(true, "GUILD_OFFICER")
+	ToggleChatColorNamesByClassGroup(true, "GUILD_ACHIEVEMENT")
+	ToggleChatColorNamesByClassGroup(true, "WHISPER")
+	ToggleChatColorNamesByClassGroup(true, "PARTY")
+	ToggleChatColorNamesByClassGroup(true, "PARTY_LEADER")
+	ToggleChatColorNamesByClassGroup(true, "RAID")
+	ToggleChatColorNamesByClassGroup(true, "RAID_LEADER")
+	ToggleChatColorNamesByClassGroup(true, "RAID_WARNING")
+	ToggleChatColorNamesByClassGroup(true, "BATTLEGROUND")
+	ToggleChatColorNamesByClassGroup(true, "BATTLEGROUND_LEADER")
+	ToggleChatColorNamesByClassGroup(true, "ACHIEVEMENT")
+	
+	-- Start with chat frame 1 shown.
+	ShowChatFrame(cft1)
+
+	-- Prevent Blizzard from changing to chat tab 1 (on instance enter, flight path end etc).
+	cf1:HookScript("OnShow", function()
+		if RecChatButtonBar.id ~= 1 then
+			ShowChatFrame(_G[format("RecChatButton%d", RecChatButtonBar.id)])
+		end
+	end)
+	
+	recUI.lib.unregisterEvent("VARIABLES_LOADED", "recUIModuleChatOnLoad")
+end)
 
 ChatTypeInfo.SAY.sticky = 1
 ChatTypeInfo.EMOTE.sticky = 1
@@ -407,9 +405,6 @@ ChatTypeInfo.RAID_WARNING.sticky = 0
 ChatTypeInfo.BATTLEGROUND.sticky = 1
 ChatTypeInfo.WHISPER.sticky = 1
 ChatTypeInfo.CHANNEL.sticky = 1
-
-f:SetScript("OnEvent", OnEvent)
-f:RegisterEvent("ADDON_LOADED")
 
 local color_delay = 10
 f:SetScript("OnUpdate", function(self, elapsed)
