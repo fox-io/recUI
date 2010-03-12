1bd4e7aa37eb5b0e82821f125a7a17a0fd2a11a8
John <john.d.mann@gmail.com>
2010-03-11 23:53:03 -0600

    Remove glow tweak.

--- modules/timers.lua
+++ modules/timers.lua
@@ -1,51 +1,53 @@
 local _, recUI = ...
 
-local recAutoShot = CreateFrame("StatusBar")
-recAutoShot:SetStatusBarTexture(recUI.media.statusBar)
-recAutoShot:SetStatusBarColor(1, .3, .3, .8)
-recAutoShot:SetPoint("CENTER")
-recAutoShot:SetSize(200, 10)
-recAutoShot:Hide()
+if recUI.lib.playerClass == "HUNTER" then
+	local recAutoShot = CreateFrame("StatusBar")
+	recAutoShot:SetStatusBarTexture(recUI.media.statusBar)
+	recAutoShot:SetStatusBarColor(1, .3, .3, .8)
+	recAutoShot:SetPoint("CENTER")
+	recAutoShot:SetSize(200, 10)
+	recAutoShot:Hide()
 
-recAutoShot.backdrop = recAutoShot:CreateTexture(nil, "BACKGROUND")
-recAutoShot.backdrop:SetPoint("TOPLEFT", -2, 2)
-recAutoShot.backdrop:SetPoint("BOTTOMRIGHT", 2, -2)
-recAutoShot.backdrop:SetTexture(recUI.media.bgFile)
-recAutoShot.backdrop:SetVertexColor(0, 0, 0, .5)
+	recAutoShot.backdrop = recAutoShot:CreateTexture(nil, "BACKGROUND")
+	recAutoShot.backdrop:SetPoint("TOPLEFT", -2, 2)
+	recAutoShot.backdrop:SetPoint("BOTTOMRIGHT", 2, -2)
+	recAutoShot.backdrop:SetTexture(recUI.media.bgFile)
+	recAutoShot.backdrop:SetVertexColor(0, 0, 0, .5)
 
-recAutoShot.label = recAutoShot:CreateFontString(nil, "OVERLAY")
-recAutoShot.label:SetPoint("LEFT", 5)
-recAutoShot.label:SetFont(recUI.media.font, 8, "THINOUTLINE")
-recAutoShot.label:SetText("Auto Shot")
+	recAutoShot.label = recAutoShot:CreateFontString(nil, "OVERLAY")
+	recAutoShot.label:SetPoint("LEFT", 5)
+	recAutoShot.label:SetFont(recUI.media.font, 8, "THINOUTLINE")
+	recAutoShot.label:SetText("Auto Shot")
 
-recAutoShot.time = recAutoShot:CreateFontString(nil, "OVERLAY")
-recAutoShot.time:SetPoint("RIGHT", -5)
-recAutoShot.time:SetFont(recUI.media.font, 8, "THINOUTLINE")
+	recAutoShot.time = recAutoShot:CreateFontString(nil, "OVERLAY")
+	recAutoShot.time:SetPoint("RIGHT", -5)
+	recAutoShot.time:SetFont(recUI.media.font, 8, "THINOUTLINE")
 
-local currentTime, startTime, endTime
-recAutoShot:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
-recAutoShot:SetScript("OnEvent", function(self, event, unit, name)
-	if unit ~= "player" then return end
-	if name ~= "Auto Shot" then return end
+	local currentTime, startTime, endTime
+	recAutoShot:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	recAutoShot:SetScript("OnEvent", function(self, event, unit, name)
+		if unit ~= "player" then return end
+		if name ~= "Auto Shot" then return end
 
-	startTime = GetTime()
-	endTime = startTime + UnitRangedDamage("player")
-	
-	self:SetMinMaxValues(startTime, endTime)
-	self.time:SetText(" ")
-	self:Show()
-end)
+		startTime = GetTime()
+		endTime = startTime + UnitRangedDamage("player")
+		
+		self:SetMinMaxValues(startTime, endTime)
+		self.time:SetText(" ")
+		self:Show()
+	end)
 
-recAutoShot:SetScript("OnUpdate", function(self, elapsed)
-	currentTime = GetTime()
-	if currentTime > endTime then
-		self:Hide()
-	else
-		local elapsed = (currentTime - startTime)
-		self:SetValue(startTime + elapsed)
-		self.time:SetFormattedText("%.1f", endTime - currentTime)
-	end
-end)
+	recAutoShot:SetScript("OnUpdate", function(self, elapsed)
+		currentTime = GetTime()
+		if currentTime > endTime then
+			self:Hide()
+		else
+			local elapsed = (currentTime - startTime)
+			self:SetValue(startTime + elapsed)
+			self.time:SetFormattedText("%.1f", endTime - currentTime)
+		end
+	end)
+end
 
 recUI.timers = {}
 local event_frame = CreateFrame("Frame")
