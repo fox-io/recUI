7e44b56710b7e127347bf626379492ab0b320c53
John <john.d.mann@gmail.com>
2010-03-12 00:07:20 -0600

    Trim!

--- modules/tweaks.lua
+++ modules/tweaks.lua
@@ -22,6 +22,15 @@ local numPage = MERCHANT_ITEMS_PER_PAGE
 local GetItemInfo = GetItemInfo
 local GetTradePlayerItemLink = GetTradePlayerItemLink
 
+
+
+
+
+
+
+
+
+
 -- Move achievement/dungeon completions to center of screen.
 local function reanchor()
 	local one, two, lfg = AchievementAlertFrame1, AchievementAlertFrame2, DungeonCompletionAlertFrame1
@@ -54,32 +63,6 @@ recUI.tweaks.eventFrame:HookScript("OnEvent", function(self, event, ...)
 	end
 end)
 
-
-
-
-
-
--- Toggle name on head when afk
-recUI.tweaks.eventFrame:RegisterEvent("CHAT_MSG_SYSTEM")
-recUI.tweaks.eventFrame:RegisterEvent("PLAYER_LOGOUT")
-recUI.tweaks.eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-recUI.tweaks.eventFrame:HookScript("OnEvent", function(self, event, ...)
-	if event == "CHAT_MSG_SYSTEM" or event == "PLAYER_LOGOUT" or event == "PLAYER_ENTERING_WORLD" then
-		if event == "PLAYER_LOGOUT" then
-			SetCVar("UnitNameOwn", 0)
-		else
-			SetCVar("UnitNameOwn", UnitIsAFK("player") and 1 or 0)
-			
-			if event == "CHAT_MSG_SYSTEM" then
-				if string.find(..., CLEARED_AFK) then
-					SetCVar("UnitNameOwn", 0)
-				elseif string.find(..., string.gsub(MARKED_AFK_MESSAGE, "%%s", ".*")) or string.find(..., MARKED_AFK) then
-					SetCVar("UnitNameOwn", 1)
-				end
-			end
-		end
-	end
-end)
 
 
 
@@ -206,101 +189,6 @@ end)
 
 -- Block errors
 UIErrorsFrame:UnregisterEvent("UI_ERROR_MESSAGE")
-
-
-
-
-
-
--- Ensure Keybinds
-local bindings = {
-                 ["W"] = "MOVEFORWARD",
-                 ["S"] = "MOVEBACKWARD",
-                 ["A"] = "TURNLEFT",
-                 ["D"] = "TURNRIGHT",
-                 ["Q"] = "STRAFELEFT",
-                 ["E"] = "STRAFERIGHT",
-                 ["1"] = "ACTIONBUTTON1",
-                 ["2"] = "ACTIONBUTTON2",
-                 ["3"] = "ACTIONBUTTON3",
-                 ["4"] = "ACTIONBUTTON4",
-                 ["5"] = "ACTIONBUTTON5",
-                 ["6"] = "ACTIONBUTTON6",
-                 ["7"] = "ACTIONBUTTON7",
-                 ["8"] = "ACTIONBUTTON8",
-                 ["9"] = "ACTIONBUTTON9",
-                 ["0"] = "ACTIONBUTTON10",
-                 ["-"] = "ACTIONBUTTON11",
-                 ["="] = "ACTIONBUTTON12",
-           ["BUTTON3"] = "MOVEANDSTEER",
-             ["SPACE"] = "JUMP",
-                 ["X"] = "SITORSTAND",
-                 ["Z"] = "TOGGLESHEATH",
-           ["NUMLOCK"] = "TOGGLEAUTORUN",
-      ["NUMPADDIVIDE"] = "TOGGLERUN",
-             ["ENTER"] = "OPENCHAT",
-                 ["/"] = "OPENCHATSLASH",
-            ["PAGEUP"] = "CHATPAGEUP",
-          ["PAGEDOWN"] = "CHATPAGEDOWN",
-    ["SHIFT-PAGEDOWN"] = "CHATBOTTOM",
-                 ["R"] = "REPLY",
-           ["SHIFT-R"] = "REPLY2",
-               ["TAB"] = "TARGETNEARESTENEMY",
-         ["SHIFT-TAB"] = "TARGETLASTENEMY",
-          ["CTRL-TAB"] = "TARGETNEARESTFRIEND",
-                ["F1"] = "TARGETSELF",
-                ["F2"] = "TARGETPARTYMEMBER1",
-                ["F3"] = "TARGETPARTYMEMBER2",
-                ["F4"] = "TARGETPARTYMEMBER3",
-                ["F5"] = "TARGETPARTYMEMBER4",
-                 ["V"] = "NAMEPLATES",
-           ["SHIFT-V"] = "FRIENDNAMEPLATES",
-            ["CTRL-V"] = "ALLNAMEPLATES",
-			     ["F"] = "ASSISTTARGET",
-                 ["C"] = "TOGGLECHARACTER0",
-                 ["B"] = "OPENALLBAGS",
-                 ["P"] = "TOGGLESPELLBOOK",
-                 ["N"] = "TOGGLETALENTS",
-                 ["H"] = "TOGGLECHARACTER4",
-                 ["K"] = "TOGGLECHARACTER1",
-                 ["L"] = "TOGGLEQUESTLOG",
-            ["ESCAPE"] = "TOGGLEGAMEMENU",
-                 ["M"] = "TOGGLEWORLDMAP",
-                 ["O"] = "TOGGLESOCIAL",
-                 ["I"] = "TOGGLELFGPARENT",
-           ["SHIFT-M"] = "TOGGLEBATTLEFIELDMINIMAP",
-                 ["Y"] = "TOGGLEACHIEVEMENT",
-       ["PRINTSCREEN"] = "SCREENSHOT",
-      ["MOUSEWHEELUP"] = "CAMERAZOOMIN",
-    ["MOUSEWHEELDOWN"] = "CAMERAZOOMOUT",
-}
-
-local event_frame = CreateFrame("Frame")
-event_frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-event_frame:SetScript("OnEvent", function(self)
-	-- Remove all keybinds
-	for i = 1, GetNumBindings() do
-		local command = GetBinding(i)
-		while GetBindingKey(command) do
-			local key = GetBindingKey(command)
-			SetBinding(key) -- Clear Keybind
-		end
-	end
-	
-	-- Apply personal keybinds
-	for key, bind in pairs(bindings) do
-		SetBinding(key, bind)
-	end
-	
-	-- Save keybinds
-	SaveBindings(1)
-
-	-- All done, clean up a bit.
-	event_frame:UnregisterEvent(event)
-	event_frame:SetScript("OnEvent", nil)
-	bindings = nil	-- Remove table
-	event_frame = nil -- Remove frame
-end)
 
 
 
@@ -490,6 +378,13 @@ recUI.tweaks.eventFrame:HookScript("OnEvent", function(self, event, ...)
 	end
 end)
 
+
+
+
+
+
+
+-- Auto repair
 recUI.tweaks.eventFrame:RegisterEvent("MERCHANT_SHOW")
 recUI.tweaks.eventFrame:HookScript("OnEvent", function(self, event, ...)
 	if event == "MERCHANT_SHOW" then
